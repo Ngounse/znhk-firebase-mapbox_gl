fb82e22780a7a53a2778d5cdf6d33aa8e42b0e3f
Ngounse HENG <hengngoungse@gmail.com>
2023-04-18 08:23:33 +0700

    auth layout 0.1.10

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-import { Box, Container } from "@mui/material";
-import { Header } from "./Header";
-import { Footer } from "./Footer";
+import React from 'react';
+import {Box, Container} from '@mui/material';
+import {Header} from './Header';
+import {Footer} from './Footer';
+import {useAuth} from 'src/context/AuthContext';
+import {useRouter} from 'next/router';
 
 export const SIDE_NAV_WIDTH = 280;
 export const TOP_NAV_HEIGHT = 64;
@@ -12,22 +14,29 @@ interface ILayoutProps {
   branch?: string;
   sx?: any;
 }
-export const Layout: React.FC<ILayoutProps> = ({ children }) => {
+export const Layout: React.FC<ILayoutProps> = ({children}) => {
+  const {currentUser} = useAuth();
+  const router = useRouter();
+
+  if (!currentUser) {
+    router.push('/auth/login');
+    return null;
+  }
+
   return (
     <Container maxWidth="lg">
-      <Box display={"flex"} flexDirection={"column"}>
+      <Box display={'flex'} flexDirection={'column'}>
         <Header />
         <Box
           sx={{
-            width: "100%",
+            width: '100%',
             height: `calc(100vh - ${TOP_NAV_HEIGHT}px - ${FOOT_NAV_HEIGHT}px)`,
-            "--nav-height": `${TOP_NAV_HEIGHT}px`,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
+            '--nav-height': `${TOP_NAV_HEIGHT}px`,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
           {children}
         </Box>
         <Footer />
